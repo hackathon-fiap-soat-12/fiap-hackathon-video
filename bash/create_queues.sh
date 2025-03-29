10a651b4793cc93de7b4d24f6953701c9c6320ac
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-29 18:43:37 -0300

    ✨ feat: add database command line

--- bash/create_queues.sh
+++ bash/create_queues.sh
@@ -22,3 +22,17 @@ echo "Todas as filas foram criadas com sucesso!"
 aws --endpoint http://localhost:4566 s3 mb s3://videofiles
 
 echo "Bucket criado com sucesso!"
+
+aws --endpoint="http://localhost:4566"  dynamodb create-table \
+    --table-name video_entity \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+        AttributeName=userId,AttributeType=S \
+    --key-schema \
+        AttributeName=id,KeyType=HASH \
+    --global-secondary-indexes \
+        "IndexName=userIdIndex,KeySchema=[{AttributeName=userId,KeyType=HASH}],Projection={ProjectionType=ALL},ProvisionedThroughput={ReadCapacityUnits=1,WriteCapacityUnits=1}" \
+    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --region us-east-1
+
+echo "DynamoDB criado com sucesso!"
