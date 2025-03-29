62f3af716fee6ccf85509db176ae8ddbbc29dee5
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-29 18:47:53 -0300

    ✨ feat: create PresignedURL on FileService S3

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/gateway/filestorage/FileService.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/gateway/filestorage/FileService.java
@@ -1,10 +1,13 @@
 package br.com.fiap.techchallenge.hackathonvideo.infra.gateway.filestorage;
 
 import java.io.InputStream;
+import java.time.Duration;
 
 public interface FileService {
 
     InputStream getFile(String bucketName, String key);
 
     Boolean uploadFile(String bucketName, String key, InputStream file);
+
+    String generatePresignedUrl(String bucketName, String key);
 }
--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/gateway/filestorage/impl/FileServiceS3Impl.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/gateway/filestorage/impl/FileServiceS3Impl.java
@@ -4,19 +4,25 @@ import br.com.fiap.techchallenge.hackathonvideo.application.exceptions.DoesNotEx
 import br.com.fiap.techchallenge.hackathonvideo.infra.gateway.filestorage.FileService;
 import io.awspring.cloud.s3.S3Template;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 
 @Service
 public class FileServiceS3Impl implements FileService {
 
     private final S3Template s3Template;
+    private final S3Presigner s3Presigner;
 
-    public FileServiceS3Impl(S3Template s3Template) {
+    public FileServiceS3Impl(S3Template s3Template, S3Presigner s3Presigner) {
         this.s3Template = s3Template;
+        this.s3Presigner = s3Presigner;
     }
 
+    @Override
     public InputStream getFile(String bucketName, String key) {
         try {
             return s3Template.download(bucketName, key).getInputStream();
@@ -25,8 +31,21 @@ public class FileServiceS3Impl implements FileService {
         }
     }
 
+    @Override
     public Boolean uploadFile(String bucketName, String key, InputStream file) {
         var uploaded = s3Template.upload(bucketName, key, file);
         return uploaded.exists();
+    }
+
+    @Override
+    public String generatePresignedUrl(String bucketName, String key) {
+        var presignedRequest = s3Presigner.presignGetObject(
+                GetObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMillis(300000L))
+                        .getObjectRequest(b -> b.bucket(bucketName).key(key))
+                        .build()
+        );
+
+        return presignedRequest.url().toString();
     }
 }
