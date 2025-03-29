678e23c79cc500dba24e7544aa98e9d0fc902163
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-29 18:47:11 -0300

    ✨ feat: create PresignedUploadUseCase

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/application/usecase/PresignedUploadUseCase.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/application/usecase/PresignedUploadUseCase.java
@@ -2,6 +2,8 @@ package br.com.fiap.techchallenge.hackathonvideo.application.usecase;
 
 import br.com.fiap.techchallenge.hackathonvideo.infra.entrypoint.controller.dto.PresignedUploadRequestDTO;
 
+import java.util.UUID;
+
 public interface PresignedUploadUseCase {
-    String presignedUpload(PresignedUploadRequestDTO dto, String userId, String email);
+    String presignedUpload(PresignedUploadRequestDTO dto, UUID userId, String email);
 }
--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/application/usecase/impl/PresignedUploadUseCaseImpl.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/application/usecase/impl/PresignedUploadUseCaseImpl.java
@@ -1,12 +1,27 @@
 package br.com.fiap.techchallenge.hackathonvideo.application.usecase.impl;
 
+import br.com.fiap.techchallenge.hackathonvideo.application.persistence.VideoPersistence;
 import br.com.fiap.techchallenge.hackathonvideo.application.usecase.PresignedUploadUseCase;
+import br.com.fiap.techchallenge.hackathonvideo.domain.models.Video;
 import br.com.fiap.techchallenge.hackathonvideo.infra.entrypoint.controller.dto.PresignedUploadRequestDTO;
+import br.com.fiap.techchallenge.hackathonvideo.infra.gateway.filestorage.FileService;
+
+import java.util.UUID;
 
 public class PresignedUploadUseCaseImpl implements PresignedUploadUseCase {
 
+    private final VideoPersistence videoPersistence;
+    private final FileService fileService;
+
+    public PresignedUploadUseCaseImpl(VideoPersistence videoPersistence, FileService fileService) {
+        this.videoPersistence = videoPersistence;
+        this.fileService = fileService;
+    }
+
     @Override
-    public String presignedUpload(PresignedUploadRequestDTO dto, String userId, String email) {
-        return "";
+    public String presignedUpload(PresignedUploadRequestDTO dto, UUID userId, String email) {
+        var video = videoPersistence.create(new Video(dto.fileName(), userId, email));
+
+        return fileService.generatePresignedUrl(video.getBucketName(), video.getVideoKey());
     }
 }
