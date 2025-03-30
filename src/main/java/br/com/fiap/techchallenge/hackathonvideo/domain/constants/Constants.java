1978242d55985fffec312476c7f69910f0e68a44
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-29 21:56:53 -0300

    ✨ feat: implement new constants for s3 paths

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/domain/constants/Constants.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/domain/constants/Constants.java
@@ -2,6 +2,9 @@ package br.com.fiap.techchallenge.hackathonvideo.domain.constants;
 
 public class Constants {
     public static final String BUCKET_NAME = "videofiles";
+    private static final String PATH_BASE = "/";
+    public static final String PATH_VIDEO = PATH_BASE + "videos/";
+    public static final String PATH_FRAMES = PATH_BASE + "frames/";
 
     private Constants() {}
 }
