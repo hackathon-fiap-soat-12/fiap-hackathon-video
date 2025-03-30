ce50b77c0209b5bd4bafef07835f5ab7056bd9df
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-30 18:49:08 -0300

    ✨ feat: create new constants

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/domain/constants/Constants.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/domain/constants/Constants.java
@@ -1,10 +1,11 @@
 package br.com.fiap.techchallenge.hackathonvideo.domain.constants;
 
 public class Constants {
-    public static final String BUCKET_NAME = "videofiles";
     private static final String PATH_BASE = "/";
-    public static final String PATH_VIDEO = PATH_BASE + "videos/";
-    public static final String PATH_FRAMES = PATH_BASE + "frames/";
+    public static final String BUCKET_NAME = "videofiles";
+    public static final String PATH_VIDEO = BUCKET_NAME + PATH_BASE + "videos/";
+    public static final String PATH_FRAMES = BUCKET_NAME + PATH_BASE + "frames/";
+    public static final String PUT_METHOD = "PUT";
 
     private Constants() {}
 }
