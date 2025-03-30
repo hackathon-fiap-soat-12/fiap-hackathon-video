336bce2a36261f97f8578e699bf714cb01025672
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-03-29 21:58:11 -0300

    ✨ fix: reformulate dtos constructors

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/entrypoint/controller/dto/PresignedDownloadResponseDTO.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/entrypoint/controller/dto/PresignedDownloadResponseDTO.java
@@ -2,4 +2,7 @@ package br.com.fiap.techchallenge.hackathonvideo.infra.entrypoint.controller.dto
 
 public record PresignedDownloadResponseDTO(String url,
                                            String method) {
+    public PresignedDownloadResponseDTO(String url) {
+        this(url, "GET");
+    }
 }
--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/entrypoint/controller/dto/PresignedUploadResponseDTO.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/entrypoint/controller/dto/PresignedUploadResponseDTO.java
@@ -1,6 +1,11 @@
 package br.com.fiap.techchallenge.hackathonvideo.infra.entrypoint.controller.dto;
 
+import br.com.fiap.techchallenge.hackathonvideo.domain.models.PresignedFile;
+
 public record PresignedUploadResponseDTO(String url,
                                          String method,
                                          Integer expiresIn) {
+    public PresignedUploadResponseDTO(PresignedFile presignedFile) {
+        this(presignedFile.getUrl(), "PUT", presignedFile.getSecondsToExpire());
+    }
 }
