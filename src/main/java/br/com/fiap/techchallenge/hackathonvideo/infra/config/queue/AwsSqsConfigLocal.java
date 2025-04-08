d9c8bc066db2d51bc7f59db5e4b749154f6e21f9
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-04-08 20:44:51 -0300

    ✨ feat: Create profiles to provide correct beans

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/config/queue/AwsSqsConfigLocal.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/infra/config/queue/AwsSqsConfigLocal.java
@@ -1,21 +1,28 @@
 package br.com.fiap.techchallenge.hackathonvideo.infra.config.queue;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 import java.net.URI;
 
 @Configuration
-@Profile("!local")
-public class AwsSqsConfig {
+@Profile("local")
+public class AwsSqsConfigLocal {
+
+    @Value("${aws.url:default}")
+    private String awsUrl;
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
         return SqsAsyncClient.builder()
+                .endpointOverride(URI.create(awsUrl))
                 .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 }
