2d2d5095de9e2a1498fb997bd7e488b9729fa631
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-04-08 19:27:39 -0300

    🧪 test: add PresignedUploadUseCaseConfigTest

---
+++ src/test/java/br/com/fiap/techchallenge/hackathonvideo/infra/config/bean/PresignedUploadUseCaseConfigTest.java
@@ -0,0 +1,38 @@
+package br.com.fiap.techchallenge.hackathonvideo.infra.config.bean;
+
+import br.com.fiap.techchallenge.hackathonvideo.application.usecase.impl.PresignedUploadUseCaseImpl;
+import br.com.fiap.techchallenge.hackathonvideo.infra.gateway.database.repository.impl.VideoPersistenceImpl;
+import br.com.fiap.techchallenge.hackathonvideo.infra.gateway.filestorage.FileServiceS3Impl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class PresignedUploadUseCaseConfigTest {
+
+    @Mock
+    private VideoPersistenceImpl videoPersistence;
+
+    @Mock
+    private FileServiceS3Impl fileService;
+
+    @InjectMocks
+    private PresignedUploadUseCaseConfig presignedUploadUseCaseConfig;
+
+    @Test
+    @DisplayName("Should Create a Singleton Instance Of PresignedUploadUseCaseImpl")
+    void shouldCreateSingletonInstanceOfPresignedUploadUseCaseImpl() {
+        var presignedUploadUseCaseImpl = presignedUploadUseCaseConfig.presignedUploadUseCase(videoPersistence, fileService);
+
+        assertNotNull(presignedUploadUseCaseImpl);
+        assertNotNull(videoPersistence);
+        assertNotNull(fileService);
+        assertInstanceOf(PresignedUploadUseCaseImpl.class, presignedUploadUseCaseImpl);
+    }
+
+}
