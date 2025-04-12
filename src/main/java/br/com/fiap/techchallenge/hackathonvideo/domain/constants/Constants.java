1cfe6ede66361a922d28bb4b4a1a057e1154a2fc
Rodrigo Sartori <rodrigo.sartori00@yahoo.com>
2025-04-12 00:58:55 -0300

    ✨ feat: change bucket name

--- src/main/java/br/com/fiap/techchallenge/hackathonvideo/domain/constants/Constants.java
+++ src/main/java/br/com/fiap/techchallenge/hackathonvideo/domain/constants/Constants.java
@@ -3,8 +3,8 @@ package br.com.fiap.techchallenge.hackathonvideo.domain.constants;
 public class Constants {
     private static final String PATH_BASE = "/";
     public static final String BUCKET_NAME = "fiap-alquimia-dos-frames";
-    public static final String PATH_VIDEO = BUCKET_NAME + PATH_BASE + "videos/";
-    public static final String PATH_FRAMES = BUCKET_NAME + PATH_BASE + "frames/";
+    public static final String PATH_VIDEO = "videos/";
+    public static final String PATH_FRAMES = "frames/";
 
     private Constants() { }
 }
